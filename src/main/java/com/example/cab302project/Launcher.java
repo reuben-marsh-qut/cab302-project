4c9c0a999ef39caeb609662a74dbd45898d0dccf
reubenmarsh <n11964464@qut.edu.au>
2026-08-10 12:29:48 +1000

    Initial commit

---
+++ src/main/java/com/example/cab302project/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.cab302project;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
