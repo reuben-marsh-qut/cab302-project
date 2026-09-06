62d6cbf0abd69cb6a21a439d845f463f67d5aa48
GloballyUniquePlaceholder <265477880+GloballyUniquePlaceholder@users.noreply.github.com>
2026-09-06 17:21:01 +1000

    set title

--- SustainabilityProject/src/main/java/com/example/cab302project/HelloApplication.java
+++ SustainabilityProject/src/main/java/com/example/cab302project/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 732*2, 412*2);
+        stage.setTitle("Rooted: Water your mind");
         stage.setScene(scene);
         stage.show();
     }
